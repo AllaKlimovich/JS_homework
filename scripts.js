506d89882faaac40d60a6612bd25b0aff83f0c29
Alla <Alla30052007@mail.ru>
2022-08-06 17:30:38 +0300

    start DZ_1

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//задание 2
+
+console.log('№ 2');          
+let a1 = 5 % 3,
+    a2 = 3 % 5,
+    a3 = 5 + '3',
+    a4 = '5' - 3,
+    a5 = 75 + "кг";
+console.log(a1,typeof(a1));
+console.log(a2,typeof(a2));
+console.log(a3,typeof(a3));
+console.log(a4,typeof(a4));
+console.log(a5,typeof(a5));
